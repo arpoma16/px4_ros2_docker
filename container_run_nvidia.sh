3e2fd49706b3d4c8683f7e11428bbb45f6ba77b0
arpoma16 <arpoma167@gmail.com>
2025-10-15 18:12:30 +0200

    fix warning qt to display

--- container_run_nvidia.sh
+++ container_run_nvidia.sh
@@ -3,8 +3,12 @@
 export CONTAINER_NAME="px4_ros2_cont"
 export IMAGE_NAME="px4-ros2:v1.0"
 
+#export XDG_RUNTIME_DIR=/tmp/runtime-root
+#mkdir -p $XDG_RUNTIME_DIR
+#chmod 0700 $XDG_RUNTIME_DIR
+xhost +local:root
 
-xhost +local:docker;
+#xhost +local:docker;
 
 PROJECT_DIR="${HOME}/work/px4_volume";
 PROJECT_DIST="/home/grvc/ros2_ws/src/external";
@@ -16,19 +20,21 @@ if [ "$(docker ps -qaf name=$CONTAINER_NAME)" = "" ]; then
     --name $CONTAINER_NAME \
     --privileged \
     --workdir $PROJECT_DIST \
-    --gpus all \
     --env DISPLAY=$DISPLAY \
+    --volume /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    --env XAUTHORITY=$XAUTHORITY \
+    --volume $XAUTHORITY:$XAUTHORITY \
     --env QT_X11_NO_MITSHM=1 \
     --env NVIDIA_VISIBLE_DEVICES=all \
     --env NVIDIA_DRIVER_CAPABILITIES=all \
-    --env XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR} \
+    --env XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
     --env __NV_PRIME_RENDER_OFFLOAD=1 \
     --env __GLX_VENDOR_LIBRARY_NAME=nvidia \
-    --env XAUTHORITY=${XAUTHORITY} \
+    --device /dev/dri:/dev/dri \
+    --volume /etc/machine-id:/etc/machine-id:ro \
+    --gpus all \
     --network host \
     --ipc=host \
-    --volume /tmp/.X11-unix:/tmp/.X11-unix \
-    --volume $XAUTHORITY:$XAUTHORITY \
     --mount type=bind,source=$PROJECT_DIR,destination=$PROJECT_DIST \
     $IMAGE_NAME bash;
 
