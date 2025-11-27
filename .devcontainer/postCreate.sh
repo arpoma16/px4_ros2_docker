08b4bdd526f937d99573bb801709b2aa67678ed6
arpoma16 <arpoma167@gmail.com>
2025-11-27 16:46:56 +0100

    add .devcontainer

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+apt update
+
+rosdep update
+cd /root/ros2_ws/
+rm -rf build/ install/
+export PIP_BREAK_SYSTEM_PACKAGES=1
+rosdep install --from-paths src --ignore-src -r -y | grep "apt-get install" | sed 's/.*apt-get install //' > /tmp/ros_dependencies.txt
+colcon build --symlink-install
+#######
+# Config ZSH
+#######
+# https://www.josephguadagno.net/2025/03/27/add-and-customize-oh-my-zsh-in-a-linux-development-container
+# powerline fonts for zsh theme
+apt update
+apt install --only-upgrade ros-humble-rosbridge-library ros-humble-rosbridge-server -y
+
+cd ~
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+cd .. && rm -rf fonts
+
+# oh-my-zsh plugins
+
+zsh -c 'git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k'
+cd /root/ros2_ws/src/external/
+cp .devcontainer/dotfiles/.zshrc ~
+cp .devcontainer/dotfiles/.p10k.zsh ~
